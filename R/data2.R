6068d7a82d548527535b6e1fb5c9990a8ee202b3
iriszy <zhangye920608@gmail.com>
2018-12-13 19:20:21 -0500

    final

---
+++ R/data2.R
@@ -0,0 +1,55 @@
+#' Hong Kong public opinion data (data frame)
+#'
+#' A data frame based on 39 survey tables from the Public Opinion Porgramme (POP)
+#' at the University of Hong Kong. Each row is a half-yearly summary of surveys
+#' on different topics.
+#'
+#' @format A data frame with 53 rows and 48 columns:
+#' \describe{
+#'   \item{CE_name}{Name of Chief Executive}
+#'   \item{CE_score}{Rating of Chief Executive, 0-100, 0 indicating the most negative}
+#'   \item{gov_overall}{Rating of overall performance of HK government, 1-5, 1 indicating the most negative}
+#'   \item{gov_econ}{Rating of HK government on economic growth, 1-5, 1 indicating the most negative}
+#'   \item{gov_live}{Rating of HK government on improving livelihood, 1-5, 1 indicating the most negative}
+#'   \item{gov_demo}{Rating of HK government on democratization, 1-5, 1 indicating the most negative}
+#'   \item{gov_human_rights}{Rating of HK government on protecting human rights and freedom, 1-5, 1 indicating the most negative}
+#'   \item{gov_china}{Rating of HK government on handling its relation with central government, 1-5, 1 indicating the most negative}
+#'   \item{hk_future}{People's confidence in HK's future, net value, percentage of confident minus percentage of not confident}
+#'   \item{china_future}{People's confidence in China's Future, net value, percentage of confident minus percentage of not confident}
+#'   \item{system}{People's confidence in one-country-two-systems, net value, percentage of confident minus percentage of not confident}
+#'   \item{democracy}{Degree of democracy, 0-10, 0 indicating the most negative}
+#'   \item{freedom}{Degree of freedom, 0-10, 0 indicating the most negative}
+#'   \item{rule_of_law}{Compliance with the rule of law, 0-10, 0 indicating the most negative}
+#'   \item{prosperity}{Degree of prosperity, 0-10, 0 indicating the most negative}
+#'   \item{stability}{Degree of stability, 0-10, 0 indicating the most negative}
+#'   \item{fairness}{Degree of fairness, 0-10, 0 indicating the most negative}
+#'   \item{civilization}{Degree of civilization, 0-10, 0 indicating the most negative}
+#'   \item{corruption_free}{Degree of corruption-free, 0-10, 0 indicating the most negative}
+#'   \item{equality}{Degree of equality, 0-10, 0 indicating the most negative}
+#'   \item{efficiency}{Degree of efficiency, 0-10, 0 indicating the most negative}
+#'   \item{social_welfare}{Sufficiency of social welfare, 0-10, 0 indicating the most negative}
+#'   \item{public_order}{Degree of public order, 0-10, 0 indicating the most negative}
+#'   \item{free_speech}{Freedom of speech, 0-10, 0 indicating the most negative}
+#'   \item{free_press}{Freedom of press, 0-10, 0 indicating the most negative}
+#'   \item{free_publication}{Freedom of publication, 0-10, 0 indicating the most negative}
+#'   \item{free_protest}{Freedom of procession and demonstration, 0-10, 0 indicating the most negative}
+#'   \item{free_association}{Freedom of sssociation, 0-10, 0 indicating the most negative}
+#'   \item{free_strike}{Freedom to strike, 0-10, 0 indicating the most negative}
+#'   \item{free_move}{Freedom to Enter or Leave Hong Kong, 0-10, 0 indicating the most negative}
+#'   \item{free_academic}{Freedom to engage in academic research, 0-10, 0 indicating the most negative}
+#'   \item{free_art}{Freedom to engage in artistic/literary creation, 0-10, 0 indicating the most negative}
+#'   \item{free_religion}{Freedom of religious belief, 0-10, 0 indicating the most negative}
+#'   \item{media_credibility}{Credibility rating of the news media, 0 indicating the most negative}
+#'   \item{hker}{percentage of people who identify themselves as Hongkonger}
+#'   \item{hker_in_china}{percentage of people who identify themselves as Hongkonger in China}
+#'   \item{chinese_in_hk}{percentage of people who identify themselves as Chinese in Hong Kong}
+#'   \item{chinese}{percentage of people who identify themselves as Chinese}
+#'   \item{mixed_identity}{percentage of people who think they have mixed identities}
+#'   \item{very_trust_cg}{percentage of people who think they very trust central government}
+#'   \item{quite_trust_cg}{percentage of people who think they quite trust central government}
+#'   \item{half-half}{percentage of people who think their trust in central government is half-half}
+#'   \item{quite_distrust_cg}{percentage of people who think they quite distrust central government}
+#'   \item{very_distrust_cg}{percentage of people who think they very distrust central government}
+#' }
+#' @source \url{https://www.hkupop.hku.hk/english/popexpress/index.html}
+"pop_score"
---
+++ R/hkpop.R
@@ -0,0 +1,68 @@
+
+#' Get Hong Kong Public Opinion Data.
+#'
+#' This function allows the user to get Hong Kong public opinion data from the
+#' Public Opinion Programme (POP) survey center at the University of Hong Kong.
+#'
+#' The function asks the user to enter the url of a particular survey from the
+#' POP website: \url{https://www.hkupop.hku.hk/english/popexpress/index.html}.
+#' It works on selected surveys: Popularity of Chief Executive (half-yearly only);
+#' Popularity of the Government; Appraisal of Current Conditions; Social Indicators;
+#' Freedom Indicators; Rule of Law Indicators; Media Performance; People's Ethnic Identity;
+#' some National Issues.
+#'
+#' @param url the url of a particular survey
+#' @usage hkpop(url)
+#' @return a data frame of the survey table which can be worked with in R
+#' @examples
+#' hkpop("https://www.hkupop.hku.hk/english/popexpress/sargperf/sarg/halfyr/datatables.html")
+#' @export
+
+
+hkpop <- function(url = "https://www.hkupop.hku.hk/english/popexpress/sargperf/sarg/halfyr/datatables.html") {
+  library(rvest)
+  library(dplyr)
+  library(stringr)
+  if (url %in% c(
+    "https://www.hkupop.hku.hk/english/popexpress/ce2017/cl/halfyr/datatables.html",
+    "https://www.hkupop.hku.hk/english/popexpress/ce2012/cy/halfyr/datatables.html",
+    "https://www.hkupop.hku.hk/english/popexpress/ce2005/donald_new/halfyr/datatables.html")) {
+    pop <- read_html(url)
+    pop_node <- html_node(pop, xpath = '//*[@id="popexpress"]/span/span/table')
+    pop_table <- html_table(pop_node)
+    colnames_pop <- pop_table[1, ] %>% str_remove_all("[^A-Za-z\\s()-]")
+    colnames(pop_table) <- colnames_pop
+    pop_table <- pop_table[-1, ]
+    pop_table[, 1] <- pop_table[, 1] %>% str_remove_all("[^0-9/-]")
+    pop_table[, 2:ncol(pop_table)] <- as.data.frame(gsub("[^0-9\\.%-]", "",
+                                                         as.matrix(pop_table[, 2:ncol(pop_table)])))
+    for (i in 2:ncol(pop_table)) {
+      if (str_detect(pop_table[, i], "%")) {
+        pop_table[, i] <- as.numeric(sub("%","", pop_table[, i]))/100
+      } else {
+        pop_table[, i] <- as.numeric(as.character(pop_table[, i]))
+      }
+    }
+    return(pop_table)
+  } else {
+    pop <- read_html(url)
+    pop_node <- html_node(pop, xpath = '//*[@id="popexpress"]/table')
+    pop_table <- html_table(pop_node)
+    colnames_pop <- pop_table[1, ] %>% str_remove_all("[^A-Za-z\\s()-]")
+    colnames(pop_table) <- colnames_pop
+    pop_table <- pop_table[-1, ]
+    pop_table[, 1] <- pop_table[, 1] %>% str_remove_all("[^0-9/-]")
+    pop_table[, 2:ncol(pop_table)] <- as.data.frame(gsub("[^0-9\\.%-]", "",
+                                                         as.matrix(pop_table[, 2:ncol(pop_table)])))
+    for (i in 2:ncol(pop_table)) {
+      if (str_detect(pop_table[, i], "%")) {
+        pop_table[, i] <- as.numeric(sub("%","", pop_table[, i]))/100
+      } else {
+        pop_table[, i] <- as.numeric(as.character(pop_table[, i]))
+      }
+    }
+    return(pop_table)
+  }
+}
+
+
